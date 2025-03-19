e11c75a43ed14ab43955137616ecf8aa0854ef63
CMD-Golem <64982383+CMD-Golem@users.noreply.github.com>
2025-03-19 19:07:42 +0100

    use classic cas, geogebra hosted lib, dark canvas

--- src/src/js/main.js
+++ src/src/js/main.js
@@ -1,15 +1,32 @@
 // "showLogging": true,
+var ggbApplet
 
-const ggbApp = new GGBApplet({
-	"appName": "CAS",
-	"width": 800,
-	"height": 800,
-	"editorBackgroundColor": "#fff",
-	"editorForegroundColor": "#000",
-	"detachKeyboard": true,
-	"showAlgebraInput": true,
-	"borderColor": ""
-}, true);
-ggbApp.setHTML5Codebase('src/geogebra/HTML5/5.0/web3d/');
+async function load() {
+	var response = await fetch("src/ggb_base64.ggb");
+	var arrayBuffer = await response.arrayBuffer();
 
-ggbApp.inject("ggb-element");
+	var binary = new Uint8Array(arrayBuffer).reduce((acc, byte) => acc + String.fromCharCode(byte), '');
+	var base64 = btoa(binary);
+
+	ggbApplet = new GGBApplet({
+		appName: "classic",
+		width: 800,
+		height: 800,
+		editorBackgroundColor: "#fff",
+		editorForegroundColor: "#000",
+		detachKeyboard: true,
+		showAlgebraInput: true,
+		borderColor: "",
+		ggbBase64: base64
+	}, true);
+
+	// ggbApplet.setHTML5Codebase('src/geogebra/HTML5/5.0/web3d/');
+	ggbApplet.inject("ggb-element");
+}
+
+load()
+
+
+
+
+
