f2140d674e267b3a489b6dee351881d5346e1c41
CMD-Golem <64982383+CMD-Golem@users.noreply.github.com>
2026-02-18 23:00:56 +0100

    translation, improved center math data

--- src/src/js/translation.js
+++ src/src/js/translation.js
@@ -1,35 +1,31 @@
 const undef = {length:null, insert:null};
-var limit = 0;
 
 function translation(input) {
 	if (input[0] == "(" && input[input.length - 1] == ")") input = input.slice(1,-1);
 
-	console.log(input)
-
 	var output = "";
 	var lastIndex = 0;
 	var match;
-	var regex = new RegExp(translation_regex, "g")
-
-	if (limit >= 10) return;
-	limit++;
+	var regex = new RegExp(translation_regex, "g");
 
 	while ((match = regex.exec(input)) !== null) {
 		var found = match[0];
 		var pos = match.index;
 
+		if (pos < lastIndex) continue;
+
 		console.log(match)
 
 		// add text before match
 		output += input.slice(lastIndex, pos);
-		var rule = translation_layer[found];
+		var rule = translation_layer[match];
 
 		if (typeof rule == "function") var {length, insert} = rule(pos, input);
 		else {
 			var length = found.length;
 			var insert = rule;
 		}
-		if (insert == null) return;
+		if (insert == null) return "";
 
 		output += insert;
 		lastIndex = pos + length;
@@ -80,34 +76,28 @@ function sum(pos, str) {
 	var top_bracket_end = getBracketLength(split_str, bottom_bracket_end + 2);
 	var sum_end = getBracketLength(split_str, top_bracket_end + 1);
 
-	var bottom_bracket_content = removeBrackets(split_str.slice(0, bottom_bracket_end + 1).join(""));
-	var top_bracket_content = removeBrackets(split_str.slice(bottom_bracket_end + 2, top_bracket_end + 1).join(""));
-	var sum_content = removeBrackets(split_str.slice(top_bracket_end + 1, sum_end + 1).join(""));
+	var bottom_bracket_content = translation(split_str.slice(0, bottom_bracket_end + 1).join(""));
+	var top_bracket_content = translation(split_str.slice(bottom_bracket_end + 2, top_bracket_end + 1).join(""));
+	var sum_content = translation(split_str.slice(top_bracket_end + 1, sum_end + 1).join(""));
 	var length = sum_end + value_pos + 1 - pos;
 
 	var bottom_bracket_array = bottom_bracket_content.split("=");
 	if (bottom_bracket_array.length != 2 || bottom_bracket_array[1] == "") return undef;
 
 	return {length: length, insert: `Sum(${sum_content}, ${bottom_bracket_array[0]}, ${bottom_bracket_array[1]}, ${top_bracket_content})`};
-
-	console.log("Sum: ", sum_content, sum_end)
-	console.log("bottom: ", bottom_bracket_content, bottom_bracket_end)
-	console.log("top: ", top_bracket_content, top_bracket_end)
-	console.log("complete: ", complete)
 }
 
 function nroot(pos, str) {
-	// root(3)(9) to nroot(x, n)
+	// root(n)(x) to nroot(x, n)
 	var split_str = str.slice(pos).split("");
 
 	var n_bracket_end = getBracketLength(split_str, 4);
 	var x_bracket_end = getBracketLength(split_str, n_bracket_end + 1);
 
-	var n_content = translation(split_str.slice(5, n_bracket_end + 1).join(""));
+	var n_content = translation(split_str.slice(5, n_bracket_end).join(""));
 	var x_content = translation(split_str.slice(n_bracket_end + 1, x_bracket_end + 1).join(""));
-	var length = x_bracket_end + 5 - pos;
+	var length = x_bracket_end + 1 - pos;
 
-	return {length: length, insert: `nroot(${n_content}, ${x_content})`};
+	return {length: length, insert: `nroot(${x_content}, ${n_content})`};
 }
 
-// var {search, replace} = sum(0, "sum  _(x=0)^2(2+5)(2x)") 
